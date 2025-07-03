f4b6f28ba258970bf064ea69f7de991acedb59b6
Vedant <vedantdumbre0706@gmail.com>
2025-07-03 20:18:16 +0530

    made cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,29 +7,31 @@ const Main = () => {
     <div className="main">
       <div className="nav">
         <p className="nav-title">Gemini</p>
-        <img className="user-icon" src={assets.gemini_icon} alt="gemini Icon" />
+        <img className="user-icon" src={assets.user_icon} alt="User Icon" />
       </div>
+
       <div className="main-container">
         <div className="greet">
-          <p><span> Hello, Ved.</span></p>
-          <p>How can I help you today?</p>
+          <p className="greet-hello">Hello, <span>Dev.</span></p>
+          <p className="greet-sub">How can I help you today?</p>
         </div>
+
         <div className="cards">
           <div className="card">
-            <img src={assets.compass_icon} alt="" />
             <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="Compass Icon" />
           </div>
           <div className="card">
-            <img src={assets.bulb_icon} alt="" />
-            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="Bulb Icon" />
           </div>
           <div className="card">
-            <img src={assets.message_icon} alt="" />
-            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="Message Icon" />
           </div>
           <div className="card">
-            <img src={assets.code_icon} alt="" />
-            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="Code Icon" />
           </div>
         </div>
       </div>
